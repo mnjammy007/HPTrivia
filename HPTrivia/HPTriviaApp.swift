1042f06eecde35cbab936d445b04d5ffd2723560
mnjammy007 <naseem4954r@gmail.com>
2024-10-29 16:54:13 +0530

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Apple on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HPTriviaTests/HPTriviaTests.swift
@@ -0,0 +1,16 @@
+//
+//  HPTriviaTests.swift
+//  HPTriviaTests
+//
+//  Created by Apple on 29/10/24.
+//
+
+import Testing
+
+struct HPTriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
